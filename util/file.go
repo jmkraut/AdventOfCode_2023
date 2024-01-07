cb31d218513b3ca2bfcd5acccaea9f89a0006337
KRAUT, Jacob <jacobmatthewkraut@hotmail.com>
2024-01-07 18:35:21 +1000

    added a new method that I'll need for day 3

--- util/file.go
+++ util/file.go
@@ -13,13 +13,41 @@ func ReadFileToLines(filePath string) (*bufio.Scanner, error) {
 		return nil, err
 	}
 
+	defer fileInput.Close()
+
 	fileScanner := bufio.NewScanner(fileInput)
 	fileScanner.Split(bufio.ScanLines)
 
 	return fileScanner, nil
 }
 
-func PrintStringSplice(input []string) {
+func ReadFileTo2DStringArray(filepath string) ([][]string, error) {
+	fileInput, err := os.Open(filepath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer fileInput.Close()
+
+	fileScanner := bufio.NewScanner(fileInput)
+	var stringArray [][]string
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		stringLine := []string{}
+
+		for _, char := range line {
+			stringLine = append(stringLine, string(char))
+		}
+
+		stringArray = append(stringArray, stringLine)
+	}
+
+	return stringArray, nil
+}
+
+func PrintStringSlice(input []string) {
 	for index, line := range input {
 		fmt.Printf("[%d]: %s", index, line)
 	}
