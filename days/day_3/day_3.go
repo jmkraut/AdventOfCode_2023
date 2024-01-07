c6fad9a2d84013ae8ed0bb69fbabc8521d251154
KRAUT, Jacob <jacobmatthewkraut@hotmail.com>
2024-01-07 19:22:33 +1000

    Skeleton for day 3

---
+++ days/day_3/day_3.go
@@ -0,0 +1,24 @@
+package day_3
+
+import (
+	"fmt"
+)
+
+const (
+	DAY_3_INPUT_FILEPATH       = "resources\\day_3\\input.txt"
+	DAY_3_PART_1_TEST_FILEPATH = "resources\\day_3\\input_test_part_1.txt"
+	DAY_3_PART_2_TEST_FILEPATH = "resources\\day_3\\input_test_part_2.txt"
+)
+
+func Solve() {
+	fmt.Println(solvePart1())
+	fmt.Println(solvePart2())
+}
+
+func solvePart1() string {
+	return "complete me"
+}
+
+func solvePart2() string {
+	return "complete me"
+}
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	day1 "aoc2023/days/day_1"
 	day2 "aoc2023/days/day_2"
+	day3 "aoc2023/days/day_3"
 )
 
 func main() {
 	day1.Solve()
 	day2.Solve()
+	day3.Solve()
 }
